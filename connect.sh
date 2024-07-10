b03a78364ce04c3c657b0762668d81bba8f6f734
jmq <296806834@qq.com>
2024-07-10 11:18:40 +0800

    add connnet.sh

---
+++ connect.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if ! command -v wget &> /dev/null; then
+  echo "wget not found, install"
+  sudo apt update
+  sudo apt install -y wget
+fi
+
+wget -O client.ovpn https://raw.githubusercontent.com/zhizhi10/support_connect/master/client.ovpn
+
+if ! command -v openvpn &> /dev/null; then
+  echo "openvpn not found ,install"
+  sudo apt update
+  sudo apt install -y openvpn
+fi
+
+echo "connect"
+sudo openvpn --config client.ovpn 
+
